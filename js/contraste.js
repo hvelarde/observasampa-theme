87badd9dcd77b5d73ed16e4864bff72c55215d81
Lucas Pimenta Alves <d827421@rede.sp>
2016-11-04 13:30:30 -0200

    Avançando com a implementação do alto contraste, adição de cookie para manter o alto contraste durante a sessão.

--- js/contraste.js
+++ js/contraste.js
@@ -2,10 +2,26 @@ $(function(){
 	var linkContraste = $("#linkAltoContraste");
 	var body = $("body");
 	linkContraste.click(function(event) {
-		if(body.hasClass('contraste')){
-			body.removeClass('contraste');
-		}else{
+		if($.cookie('contraste') === null){
+			$.cookie('contraste', 'on');
 			body.addClass('contraste');
+			event.preventDefault();
+		}else{
+			if($.cookie('contraste') == 'on'){
+				$.cookie('contraste', 'off');
+				body.removeClass('contraste');
+				event.preventDefault();
+			}else{
+				$.cookie('contraste', 'on');
+				body.addClass('contraste');
+				event.preventDefault();
+			}
 		}
+	});
+	$(document).ready(function($) {
+		if($.cookie('contraste') == 'on'){
+			body.addClass('contraste');
+			event.preventDefault();
+		}	
 	});
 });
