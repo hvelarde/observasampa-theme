9743f5fa1c642f63968b3dde0eda429e710af3a4
Lucas Pimenta Alves <d827421@rede.sp>
2016-11-07 13:10:25 -0200

    correção de margins/paddings e alterações na navbar e no footer

--- js/contraste.js
+++ js/contraste.js
@@ -1,27 +1,28 @@
 $(function(){
-	var linkContraste = $("#linkAltoContraste");
-	var body = $("body");
-	linkContraste.click(function(event) {
-		if($.cookie('contraste') === null){
-			$.cookie('contraste', 'on');
-			body.addClass('contraste');
-			event.preventDefault();
-		}else{
-			if($.cookie('contraste') == 'on'){
-				$.cookie('contraste', 'off');
-				body.removeClass('contraste');
-				event.preventDefault();
-			}else{
+	$(document).ready(function($) {
+		var linkContraste = $("#linkAltoContraste");
+		var body = $("body");
+		linkContraste.click(function(event) {
+			if($.cookie('contraste') === null){
 				$.cookie('contraste', 'on');
 				body.addClass('contraste');
 				event.preventDefault();
+			}else{
+				if($.cookie('contraste') == 'on'){
+					$.removeCookie('contraste');
+					body.removeClass('contraste');
+					event.preventDefault();
+				}else{
+					$.cookie('contraste', 'on');
+					body.addClass('contraste');
+					event.preventDefault();
+				}
 			}
-		}
-	});
-	$(document).ready(function($) {
-		if($.cookie('contraste') == 'on'){
-			body.addClass('contraste');
-			event.preventDefault();
-		}	
+		});
+		
+			if($.cookie('contraste') == 'on'){
+				body.addClass('contraste');
+				event.preventDefault();
+			}	
 	});
 });
